0f9b4854791892c812b7d4c8787723eff106897c
ThanhPV <thanhpv@vtvlive.vn>
2022-05-13 00:59:41 +0700

    change config to no password and add @service anotation

--- src/main/java/com/example/GradingService/serviceimp/BaseServiceImpl.java
+++ src/main/java/com/example/GradingService/serviceimp/BaseServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.GradingService.serviceimp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.GradingService.repository.BaseRepository;
 import com.example.GradingService.service.BaseService;
 
+@Service
 public class BaseServiceImpl implements BaseService {
 	@Autowired 
 	BaseRepository baseRepository;
