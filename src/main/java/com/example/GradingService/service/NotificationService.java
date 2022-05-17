c6d0bf94ee04c37972a46e18ce405fa234495e52
ThanhPV <thanhpv@vtvlive.vn>
2022-05-17 22:20:22 +0700

    add notification service - telegram botchat

---
+++ src/main/java/com/example/GradingService/service/NotificationService.java
@@ -0,0 +1,18 @@
+package com.example.GradingService.service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+
+public class NotificationService {
+
+private static final String CHAT_ID = "-1001577190792";
+    private static final String TOKEN = "5317435312:AAHrHA8-G5Ys0EYRQy7pJjN1pJ99CYn5o9g";
+
+    public static void sendNotification(String message) {
+        TelegramBot telegramBot = new TelegramBot(TOKEN);
+        // SendMessage 9123ccb41bb8d88de3_12660501131533155990
+        System.out.println("x");
+        telegramBot.execute(new SendMessage(CHAT_ID, message));
+    }
+
+}
